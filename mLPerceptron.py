d83897db2e817092188d69db995c07c35f3bdbc6
João Gabriel Alves de Souza <jgas47855@gmail.com>
2023-11-07 17:25:07 -0300

    número de iterações reduzido

--- mLPerceptron.py
+++ mLPerceptron.py
@@ -4,7 +4,7 @@ from sklearn.metrics import mean_squared_error
 
 def mLPerceptron (trainData , trainLabel , testData , testLabel) :
   
-  perceptron = MLPRegressor (hidden_layer_sizes = (100, 50, 20,) , max_iter = 1000000 , activation = "relu").fit (trainData , trainLabel)
+  perceptron = MLPRegressor (hidden_layer_sizes = (100, 50, 20,) , max_iter = 10000 , activation = "relu").fit (trainData , trainLabel)
 
   predictions = perceptron.predict (testData)
 
