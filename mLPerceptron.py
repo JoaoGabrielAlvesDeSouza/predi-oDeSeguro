cf561cd6228e970c5cf73400a2b792dd72949006
João Gabriel Alves de Souza <jgas47855@gmail.com>
2023-11-07 20:08:58 -0300

    Update mLPerceptron.py

--- mLPerceptron.py
+++ mLPerceptron.py
@@ -1,14 +1,24 @@
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as m
 
 def mLPerceptron (trainData , trainLabel , testData , testLabel) :
-  
+
   perceptron = MLPRegressor (hidden_layer_sizes = (100, 50, 20,) , max_iter = 10000 , activation = "relu").fit (trainData , trainLabel)
 
   predictions = perceptron.predict (testData)
 
   absoluteError = mean_absolute_error (testLabel , predictions)
   squaredError = mean_squared_error (testLabel , predictions)
-  
+
+  m.title ("predições")
+  m.plot (testLabel , testLabel, label = "testes")
+  m.plot (predictions, predictions , label = "predições")
+  m.legend ()
+  m.xlabel ("testes")
+  m.ylabel ("predições")
+  m.show ()
+
+
   return absoluteError , squaredError
